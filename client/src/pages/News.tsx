339a0139009c4ce8aedd5a8b973d99fd0ba58207
Jokkim <jokkimxavier@gmail.com>
2026-08-03 16:49:05 +0530

    feat:added the navbar in the news page

--- client/src/pages/News.tsx
+++ client/src/pages/News.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react"
 import { newsApi } from "../services/newsApi"
 import type { newsResponse } from "../types/news.types";
-
+import Navbar from "../component/ui/NavBar";
+import { useSidebar } from "../hooks/useSidebar";
 
 interface Res {
     results: newsResponse[];
@@ -9,7 +10,7 @@ interface Res {
   
   export default function News() {
     const [news, setNews] = useState<newsResponse[]>([]);
-  
+   const { toggleSidebar } = useSidebar()
     useEffect(() => {
       const getNews = async () => {
         const data: Res = await newsApi();
@@ -20,15 +21,18 @@ interface Res {
     }, []);
   
     return (
+      <div>
+       <Navbar toggleSidebar={toggleSidebar}>News</Navbar>
       <div className="p-5 flex flex-wrap">
         {news.map((item) => (
-
-            <div className="p-5 w-100 h-100 border-red-100">
+          
+          <div className="p-5 w-100 h-100 border-red-100">
           <img src={item.image_url} className="w-80 h-50"/>
           <h1 className="p-2 gap-10" key={item.title}>{item.title}</h1>
             </div>
 
-        ))}
+))}
       </div>
+</div>
     );
   }

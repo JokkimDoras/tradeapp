2252d8baa844972fcc09ba414b222a15af238c92
Jokkim <jokkimxavier@gmail.com>
2026-08-03 09:34:48 +0530

    feat: implement trade news endpoint

--- client/src/pages/News.tsx
+++ client/src/pages/News.tsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react"
+import { newsApi } from "../services/newsApi"
+
 export default function News() {
+  const [ news,setNews ] = useState(null)
+  useEffect(() => {
+    const getNews = async () => {
+      const data = await newsApi();
+      setNews(data);
+    };
+  
+    getNews();
+  }, []);
+  console.log(news)
     return <div>
         News
         </div>
--- server/src/routes/newsRouter.js
+++ server/src/routes/newsRouter.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { newsController } = require('../controllers/newsController');
+const { validateNews } = require('../controllers/newsController');
 
 
 
-router.get('/:id/news')
+router.get('/trade',newsController);
+
+module.exports =  router 

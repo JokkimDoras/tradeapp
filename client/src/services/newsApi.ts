2d6c08ef0900a073d898fcf6852f044c4f216243
Jokkim <jokkimxavier@gmail.com>
2026-08-04 07:40:52 +0530

    fix:implment the authentication check in news Middlware

--- client/src/services/newsApi.ts
+++ client/src/services/newsApi.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
+import { getToken } from "../utils/auth";
 
 const API_URL = import.meta.env.VITE_API_URL 
 
 export async function newsApi () {
+    const token = getToken();
     try{
-       const res = await axios.get(`${API_URL}/api/news/trade`);
+       const res = await axios.get(`${API_URL}/api/news/trade`,{
+        headers: {
+            Authorization:`Bearer ${token}`
+        }
+       });
      
        return res.data.data
     }catch(err) {

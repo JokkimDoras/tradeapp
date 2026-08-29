0cf1c9b63c0f01f2545c2716dbb0e17c41bb7e1b
Jokkim <jokkimxavier@gmail.com>
2026-08-29 13:46:49 +0530

    feat: add reusable confirmation modal

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ErrorBoundary from './component/ErrorBoundary.tsx'
-console.log(<App/>)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
     <App />
     </ErrorBoundary>
-  </StrictMode>,
+  </StrictMode>
 )

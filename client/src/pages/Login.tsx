6ad435145a95876ef55429e9277d652f3608c527
Jokkim Doras <jokkimxavier@gmail.com>
2026-09-11 00:50:53 +0530

    feat:autofill the email in the login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router";
 import useAuth from "../hooks/useAuth";
 import { toast } from "sonner";
 import { useNavigate } from "react-router";
-import { useUser } from "../hooks/useUser";
 // import { createClient } from "@supabase/supabase-js";
 
 // export const supabase = createClient(
@@ -16,7 +15,6 @@ export default function Login() {
   const inputRef = useRef<HTMLInputElement | null>(null)
   const emailRef = useRef<HTMLInputElement | null>(null);
   const { login, loading, error } = useAuth();
-  const {user} = useUser();
   const navigate = useNavigate();
 
   useEffect(() => {
